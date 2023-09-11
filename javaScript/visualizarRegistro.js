807d48c30b243671e2cb9df513761761ce4b07f4
Isaac Victoria <isaac127@live.com.mx>
2023-09-11 00:15:41 -0600

    Versión 1
    Se reunieron más requerimientos, con los que se modificó la interfaz para las secciones "Modificar, Crear y Visualizar registro", se reduce código usando javascript, la visualización del registro se invoca mediante innerHTML y, según la sección, se modifican sus propiedades.
    Sistema preparado visualmente para manejo de datos.
    Versión 1 completada

--- javaScript/visualizarRegistro.js
+++ javaScript/visualizarRegistro.js
@@ -1,6 +1,6 @@
 function visualizar(){
     //readonly onmousedown="return false;"
-    const cuadro = document.getElementsByClassName("input");
+    const cuadro = document.getElementsByTagName("input");
     for(let i=0; i<cuadro.length;i++){
         cuadro[i].readOnly = true;
         cuadro[i].onmousedown = "return false";
