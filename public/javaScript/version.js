9f5141ea9eb96b321784ab1258ba171b5be044ca
Isaac Victoria <55108168+IsaacVB24@users.noreply.github.com>
2023-09-19 15:53:16 -0600

    Versión 1.1.1
    Corrección de estilos en los botones de todas las secciones del menú principal,. Estilo añadido: cursor: pointer;
    
    Todo listo para comenzar la parte funcional de "Crear registro".

--- public/javaScript/version.js
+++ public/javaScript/version.js
@@ -1,3 +1,3 @@
 function version(){
-    document.getElementById("contPie").innerHTML = "<p id='infoPie'>@ 2023 Copyright Isaac Victoria Benavides </p><p id='version'>Versión 1.1</p>";
+    document.getElementById("contPie").innerHTML = "<p id='infoPie'>@ 2023 Copyright Isaac Victoria Benavides </p><p id='version'>Versión 1.1.1</p>";
 }
