a245c1e749c2fe12a46797d7f7a9cbb282b7487b
Isaac Victoria <55108168+IsaacVB24@users.noreply.github.com>
2023-09-19 15:40:48 -0600

    Corrección de cabeceras
    Se modifica el código para que los enlaces tengan peticiones HTTP adecuadas, así como se configuró en el archivo "servidor.js".

--- servidor.js
+++ servidor.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const puerto = 5000; // Número de puerto que esté disponible
+const puerto = 3000; // Número de puerto que esté disponible
 // Para saber si un puerto está disponible, no se mostrará nada en el cmd después de ejecutar en Windows 'netstat -an | findstr "3000"'
 
-// Configuración para servir archivos estáticos
+// Configuración para servir archivos estáticos desde la carpeta "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Ruta de inicio
@@ -12,27 +12,18 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'html', 'main.html'));
 });
 
-// Ruta para la página de crear registro
-app.get('/crear-registro', (req, res) => {
+// Rutas para las páginas HTML
+app.get('/crearRegistro', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'html', 'crearRegistro.html'));
 });
 
-// Ruta para la página de modificación de registros
-app.get('/modificar-registro', (req, res) => {
+app.get('/modificarRegistro', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'html', 'modificarRegistro.html'));
 });
 
-// Ruta para la página principal
-app.get('/inicio', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'html', 'main.html'));
-});
-
-// Ruta para la página de visualización de registros
-app.get('/visualizar-registro', (req, res) => {
+app.get('/visualizarRegistro', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'html', 'visualizarRegistro.html'));
 });
-
-// Otras rutas y manejo de solicitudes pueden agregarse aquí
 
 // Iniciar el servidor
 app.listen(puerto, () => {
