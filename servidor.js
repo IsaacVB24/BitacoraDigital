93d8b417de816819ab5db1b278816b1ba2731aa0
Isaac Victoria <55108168+IsaacVB24@users.noreply.github.com>
2023-09-19 15:16:26 -0600

    Versión 1.1
    Se añade servidor local que corre con Express.js de Node.js, se accede al archivo mediante http://localhost:3000/

--- servidor.js
+++ servidor.js
@@ -5,12 +5,15 @@ const app = express();
 const puerto = 3000; // Número de puerto que esté disponible
 // Para saber si un puerto está disponible, no se mostrará nada en el cmd después de ejecutar en Windows 'netstat -an | findstr "3000"'
 
+// Configuración para servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Ruta de inicio
 app.get('/', (req, res) => {
-    res.send('¡Bienvenido a la Bitácora Digital!');
+    res.sendFile(path.join(__dirname, 'html', 'main.html'));
 });
 
-// Ruta para la página de creación de registros
+// Rutas para las páginas HTML
 app.get('/crear-registro', (req, res) => {
     res.sendFile(path.join(__dirname, 'html', 'crearRegistro.html'));
 });
