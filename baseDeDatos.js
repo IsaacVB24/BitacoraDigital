778efc32abeed7de0a63b046dd5ff33e2e7218e2
Isaac Victoria <55108168+IsaacVB24@users.noreply.github.com>
2023-09-19 17:47:56 -0600

    Creación DB y modulación de proyecto
    Se crea la base de datos "registrosBitacora", y se divide en módulos los archivos que corren el sistema de forma local (baseDeDatos.js, rutas.js y servidor.js)

--- baseDeDatos.js
+++ baseDeDatos.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('mi_base_de_datos.db');
+const db = new sqlite3.Database('registrosBitacora.db');
 
 db.serialize(() => {
   db.run('CREATE TABLE IF NOT EXISTS registros (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre_usuario TEXT, num_solicitud INTEGER, fecha TEXT, clave_muestra TEXT, fuentes TEXT, observaciones TEXT)');
--- servidor.js
+++ servidor.js
@@ -7,23 +7,9 @@ const puerto = 3000; // Número de puerto que esté disponible
 // Configuración para servir archivos estáticos desde la carpeta "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta de inicio
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'html', 'main.html'));
-});
-
-// Rutas para las páginas HTML
-app.get('/crearRegistro', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'html', 'crearRegistro.html'));
-});
-
-app.get('/modificarRegistro', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'html', 'modificarRegistro.html'));
-});
-
-app.get('/visualizarRegistro', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'html', 'visualizarRegistro.html'));
-});
+// Rutas de la aplicación
+const rutas = require('./rutas'); // Importa las rutas desde el archivo rutas.js
+app.use('/', rutas);
 
 // Iniciar el servidor
 app.listen(puerto, () => {
