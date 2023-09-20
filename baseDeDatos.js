e30a6a77785cc43a4f9a8f7a98e7cfafd6377c3c
Isaac Victoria <55108168+IsaacVB24@users.noreply.github.com>
2023-09-19 19:55:00 -0600

    Modificación para crear registro
    No funcional

--- baseDeDatos.js
+++ baseDeDatos.js
@@ -15,8 +15,28 @@ db.serialize(() => {
     'fecha TEXT)');
 });
 
+// Función para guardar un nuevo registro en la base de datos
+function guardarRegistro(nuevoRegistro) {
+  const stmt = db.prepare('INSERT INTO registros (nombre_usuario, num_solicitud, clave_muestra, fuentes_empleadas, duracion_analisis, tiempo_vida_filamentos, presion_camara_analisis, observaciones, fecha) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)');
+  stmt.run(
+    nuevoRegistro.nombre_usuario,
+    nuevoRegistro.num_solicitud,
+    nuevoRegistro.clave_muestra,
+    nuevoRegistro.fuentes_empleadas,
+    nuevoRegistro.duracion_analisis,
+    nuevoRegistro.tiempo_vida_filamentos,
+    nuevoRegistro.presion_camara_analisis,
+    nuevoRegistro.observaciones,
+    nuevoRegistro.fecha
+  );
+  stmt.finalize();
+}
+
 process.on('exit', () => {
   db.close();
 });
 
-module.exports = db;
+module.exports = {
+  db,
+  guardarRegistro,
+};
--- rutas.js
+++ rutas.js
@@ -1,6 +1,7 @@
+const path = require('path');
+
 const express = require('express');
 const router = express.Router();
-const path = require('path');
 const baseDeDatos = require('./baseDeDatos'); // Importa el módulo baseDeDatos.js
 
 // Página principal
@@ -21,11 +22,20 @@ router.get('/visualizarRegistro', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'html', 'visualizarRegistro.html'));
 });
 
-// Agregar más rutas según tus necesidades
-
 // Ruta para manejar la creación de registros
 router.post('/crearRegistro', (req, res) => {
-    const nuevoRegistro = req.body; // Aquí debes recuperar los datos del formulario
+    const nuevoRegistro = {
+        nombre_usuario: req.body.nomUs,
+        num_solicitud: req.body.numSol,
+        clave_muestra: req.body.clavMues,
+        fuentes_empleadas: req.body.fuenEmpl,
+        duracion_analisis: `${req.body.hdurAn}:${req.body.mdurAn}`,
+        tiempo_vida_filamentos: `${req.body.hxfti}:${req.body.mxfti}`,
+        presion_camara_analisis: req.body.presCam,
+        observaciones: req.body.observaciones,
+        fecha: new Date().toLocaleDateString(), // Obtener la fecha del sistema
+    };
+
     baseDeDatos.guardarRegistro(nuevoRegistro); // Llama a la función para guardar el registro
     res.redirect('/'); // Redirige de vuelta a la página principal
 });
