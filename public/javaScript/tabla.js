3a001c9728ad960a2b1a80197f75928816e5f5c2
Isaac Victoria <55108168+IsaacVB24@users.noreply.github.com>
2023-09-30 11:39:35 -0600

    Versión 1.2.1
    Se modifican los botones "modificar, visualizar y eliminar registro" para que, cuando el usuario seleccione al menos un registros, estos se activen y cuando sean deseleccionados, se desactiven nuevamente los tres botones.
    Sistema listo para manipular datos, próxima implementación: eliminarRegistro.

--- public/javaScript/tabla.js
+++ public/javaScript/tabla.js
@@ -1,12 +1,24 @@
 // rgb(100, 199, 160) o #64c7a0
 
 function eliminar(){
-    console.log("Hola");
     document.getElementById("emergente-eliminarR").innerHTML = "<div id='contModal'><p id='pregunta'>¿Desea realmente eliminar el registro seleccionado?</p><center><button class='btn-eliminar' id='cancelar' onclick='cancelar()'>CANCELAR</button><button class='btn-eliminar' id='eliminar' onclick='confirmar()'>SÍ, ELIMINARLO</button></center></div>";
     document.getElementById("emergente-eliminarR").style.display = "block";
 }
 
-function accionEliminar() {
+function crear(){
+    // Redirigir a crear registo
+    window.location.href = 'crearRegistro';
+}
+function visualizar(){
+    // Redirigir a la página visualizarRegistro.html
+    window.location.href = 'visualizarRegistro';
+}
+function modificar(){
+    // Redirigir a la página modificarRegistro.html
+    window.location.href = 'modificarRegistro';
+}
+
+function confirmar() {
     /*
     // Verificar si al menos un checkbox está seleccionado
     const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
@@ -25,11 +37,6 @@ function cancelar(){
     document.getElementById("emergente-eliminarR").style.display = "none";
 }
 
-function confirmar(){
-    // Registro eliminado
-}
-
-
 // Variable para almacenar los IDs de los registros seleccionados
 const registrosSeleccionados = new Set();
 
@@ -42,19 +49,67 @@ function handleCheckboxChange(event) {
     } else {
         registrosSeleccionados.delete(registroId);
     }
-
+    /*
+    <div class="botonesRegistros">
+                    <a href="/crearRegistro"><button class="btn-registros" id="crearR">Crear registro</button></a>
+                    <a href="/modificarRegistro"><button class="btn-registros" id="modificarR">Modificar registro</button></a>
+                    <a href="/visualizarRegistro"><button class="btn-registros" id="visualizarR">Visualizar registro</button></a>
+                    <button class="btn-registros" id="eliminarR" onclick="eliminar()">Eliminar registro</button>
+    */
     // Habilitar o deshabilitar el botón de eliminar según si hay registros seleccionados
-    const botonEliminar = document.getElementById('eliminarR'); // Mueve esta línea aquí
+    const botonEliminar = document.getElementById('eliminarR');
+    const botonModificar = document.getElementById('modificarR');
+    const botonVisualizar = document.getElementById('visualizarR');
+    botonEliminar.style.pointerEvents = "none";
+    botonModificar.style.pointerEvents = "none";
+    botonVisualizar.style.pointerEvents = "none";
     if (registrosSeleccionados.size > 0) {
         botonEliminar.style.backgroundColor = '#64c7a0'; // Cambia el color del botón
-        botonEliminar.style.color = '#600000'; // Cambia el color del botón
+        botonEliminar.style.color = '#600000'; // Cambia el color de letra del botón
         botonEliminar.style.pointerEvents = "all";
-        botonEliminar.style.cursor = "pointer";
         botonEliminar.disabled = false; // Habilita el botón
+        botonEliminar.addEventListener('mouseenter', function(){
+            botonEliminar.style.backgroundColor = "rgb(69, 168, 129)";
+        });
+        botonEliminar.addEventListener('mouseleave', function(){
+            botonEliminar.style.backgroundColor = "rgb(100, 199, 160)";
+        });
+
+        botonModificar.style.backgroundColor = '#64c7a0'; // Cambia el color del botón
+        botonModificar.style.color = 'black'; // Cambia el color de letra del botón
+        botonModificar.style.pointerEvents = "all";
+        botonModificar.style.cursor = "pointer";
+        botonModificar.disabled = false; // Habilita el botón
+        botonModificar.addEventListener('mouseenter', function(){
+            botonModificar.style.backgroundColor = "rgb(69, 168, 129)";
+        });
+        botonModificar.addEventListener('mouseleave', function(){
+            botonModificar.style.backgroundColor = "rgb(100, 199, 160)";
+        });
+
+        botonVisualizar.style.backgroundColor = '#64c7a0'; // Cambia el color del botón
+        botonVisualizar.style.color = 'black'; // Cambia el color de letra del botón
+        botonVisualizar.style.pointerEvents = "all";
+        botonVisualizar.style.cursor = "pointer";
+        botonVisualizar.disabled = false; // Habilita el botón
+        botonVisualizar.addEventListener('mouseenter', function(){
+            botonVisualizar.style.backgroundColor = "rgb(69, 168, 129)";
+        });
+        botonVisualizar.addEventListener('mouseleave', function(){
+            botonVisualizar.style.backgroundColor = "rgb(100, 199, 160)";
+        });
     } else {
         botonEliminar.style.backgroundColor = 'rgba(100, 199, 159, 0.400)';
         botonEliminar.style.color = 'rgba(121, 0, 0, 0.450)';
         botonEliminar.style.pointerEvents = 'none'; // Deshabilita las interacciones del botón
+        
+        botonModificar.style.backgroundColor = 'rgba(100, 199, 159, 0.400)';
+        botonModificar.style.color = 'rgba(0, 0, 0, 0.450)';
+        botonModificar.style.pointerEvents = 'none'; // Deshabilita las interacciones del botón
+        
+        botonVisualizar.style.backgroundColor = 'rgba(100, 199, 159, 0.400)';
+        botonVisualizar.style.color = 'rgba(0, 0, 0, 0.450)';
+        botonVisualizar.style.pointerEvents = 'none'; // Deshabilita las interacciones del botón
     }
 }
 
