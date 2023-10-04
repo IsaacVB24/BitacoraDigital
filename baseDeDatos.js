df8bf8ffaabec7293330a62a10d490db64a3c10b
Isaac Victoria <55108168+IsaacVB24@users.noreply.github.com>
2023-10-04 09:47:43 -0600

    Cambio de ruta de la base de datos
    Este cambio implica que la base de datos esté más segura al no publicarse en el repositorio para cuando se trabajen datos reales.

--- baseDeDatos.js
+++ baseDeDatos.js
@@ -1,5 +1,10 @@
+const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('registrosBitacora.db');
+
+// Ruta completa al archivo registrosBitacora.db
+const dbPath = path.join(__dirname, '..', 'registrosBitacora.db');
+
+const db = new sqlite3.Database(dbPath);
 
 db.serialize(() => {
   db.run('CREATE TABLE IF NOT EXISTS registros (' +
